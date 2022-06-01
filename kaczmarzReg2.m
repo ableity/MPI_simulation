71ccca92e04f1e6b2c13b1e24747144a828d7b72
ableity <1138459299@qq.com>
2022-06-01 16:28:35 +0800

    Update kaczmarzReg2.m

--- kaczmarzReg2.m
+++ kaczmarzReg2.m
@@ -1,21 +1,7 @@
 function [ x ] = kaczmarzReg2(A,b,iterations,lambd,shuff,enforceReal,enforcePositive)
-%scale down the identity matrix
-%     [m,n] = size(A);
-%     energy = rowEnergy(A);
-%     lambdZero = sum(energy.^2)/m;
-%     lambdIter = lambd*lambdZero;
-%     lambdIter = lambd;
-% 
-%     A_plus = (lambdIter)*ones(1,m);
-%     A_plus = diag(A_plus);
-%     b_plus = zeros(m,1);
-% 
-%     A = [A A_plus];
-%     b = [b;b_plus];
-%     x = kaczmarz( A,b,iterations,lambd,shuff,enforceReal,enforcePositive );
-    
-%scale right the identity matrix
-    [m,n] = size(A);
+% A has n rows and m columns（n * m）
+% b must be 1 * m, m * 1 will cause a wrong answer
+ [m,n] = size(A);
     energy = rowEnergy(A);
     lambdZero = sum(energy.^2)/m;
     lambdIter = lambd*lambdZero;
@@ -27,13 +13,13 @@ function [ x ] = kaczmarzReg2(A,b,iterations,lambd,shuff,enforceReal,enforcePosi
     A = [A;A_plus];
     x = kaczmarz( A,b,iterations,lambd,shuff,enforceReal,enforcePositive );
     x = x(1:m);
+    
     function [ energy ] = rowEnergy(A)
         % Calculate the norm of each row of the input
         % This is an image of the energy stored by each frequency component
         % but the scaling is unclear.
 
         energy = sqrt(sum(abs(A.*A),1));
-
     end
 
 end
